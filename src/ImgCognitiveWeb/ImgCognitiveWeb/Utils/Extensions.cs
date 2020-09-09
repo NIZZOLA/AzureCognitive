095675459783675d3ff8b736da3db7a61d89afb3
Marcio R Nizzola <marcio@nizzola.com.br>
2020-09-08 22:51:31 -0300

    Inclusão do Projeto e configurações.

---
+++ src/ImgCognitiveWeb/ImgCognitiveWeb/Utils/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImgCognitiveWeb.Utils
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsImage( this IFormFile file )
+        {
+            string[] extensions = new string[] { ".JPG", ".GIF", ".BMP", ".JPEG", ".PNG", ".TIF" };
+            foreach (var item in extensions)
+            {
+                if (file.FileName.ToUpper().Contains(item))
+                    return true;
+            }
+            return false;
+        }
+
+        public static string ToPercent( this double value )
+        {
+            return (value * 100).ToString("##0.000") + " %";
+        }
+    }
+}
